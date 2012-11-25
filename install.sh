d55820c0081a890f675cdfefccb5f640d835eb90
Shafeen Tejani <shafeen.tejani@gmail.com>
2012-11-25 12:13:31 +0000

    fixed syntax error in install script

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ do
     SRC="$PWD/$FILE"
     DEST="$HOME/.$FILE"
     if [ -f $DEST ]; then
-        if [ ! -L $DEST]; then
+        if [ ! -L $DEST ]; then
             echo "fatal: $DEST already exists"
             exit 1
         else
